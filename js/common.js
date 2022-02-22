95f5316caea4e1288675ad09376cfba4b9fe03fb
minhyepark <minhyepark.dev@gmail.com>
2022-02-22 14:56:23 +0900

    메뉴에 tab했을때 기능 추가

--- js/common.js
+++ js/common.js
@@ -1,7 +1,12 @@
-// menu
 const gnb_lis = document.querySelectorAll("#gnb > li");
+const goTop = document.querySelector("#go-top");
+const btnCall = document.querySelector(".btn-call");
+const menuMobile = document.querySelector(".nav-mobile");
+const subTitle = document.querySelector(".title-wrap h2");
+
+// 메뉴 버튼에 마우스오버시 서브메뉴 표출
 gnb_lis.forEach((li) => {
-    li.addEventListener("mouseenter", (e) => {
+    li.querySelector("a").addEventListener("mouseenter", (e) => {
         li.classList.add("on");
         const sub = e.currentTarget.closest("li").querySelector(".sub");
         if (sub) {
@@ -15,20 +20,32 @@ gnb_lis.forEach((li) => {
             sub.style.zIndex = 2;
         }
     });
+    li.addEventListener("focusin", (e) => {
+        li.classList.add("on");
+        const sub = e.currentTarget.closest("li").querySelector(".sub");
+        if (sub) {
+            sub.style.zIndex = 3;
+        }
+    });
+    li.addEventListener("focusout", (e) => {
+        li.classList.remove("on");
+    });
+    const sub = li.querySelector(".sub ul");
+    if (sub) {
+        const lastEl = sub.lastElementChild;
+        lastEl.addEventListener("focusout", (e) => {
+            e.currentTarget.closest(".sub").style.zIndex = 0;
+        });
+    }
 });
 
-// go to top
-const goTop = document.querySelector("#go-top");
-
+// 상단으로 이동하는 버튼 클릭 시
 goTop.addEventListener("click", (e) => {
     e.preventDefault();
     window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
-// mobile menu
-const btnCall = document.querySelector(".btn-call");
-const menuMobile = document.querySelector(".nav-mobile");
-
+// 모바일 햄버거 메뉴 클릭시
 btnCall.onclick = function (e) {
     e.preventDefault();
     btnCall.classList.toggle("on");
@@ -36,6 +53,5 @@ btnCall.onclick = function (e) {
     body.classList.toggle("on");
 };
 
-// sub page h2 line
-const subTitle = document.querySelector(".title-wrap h2");
+// 서브페이지가 로딩되었을 때
 if (subTitle) subTitle.classList.add("on");
