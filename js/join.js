8a61b67643f91032aca8b77bc3390aef894ed009
minhyepark <minhyepark.dev@gmail.com>
2022-02-22 14:58:03 +0900

    중복코드 제거

--- js/join.js
+++ js/join.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("#member");
 const btnSubmit = form.querySelector("input[type=submit]");
 
+// submit 버튼 클릭시 모든 항목에 에러가 없는지 인증
 btnSubmit.addEventListener("click", (e) => {
     //userid 인증함수
     if (!isTxt("userid", 5)) e.preventDefault();
@@ -12,24 +13,22 @@ btnSubmit.addEventListener("click", (e) => {
     if (!isCheck("gender")) e.preventDefault();
     //hobby 인증함수
     if (!isCheck("hobby")) e.preventDefault();
-    //comments 인증
+    //comments 인증함수
     if (!isTxt("comments", 20)) e.preventDefault();
 });
 
+// 텍스트 인증 함수 정의
 function isTxt(name, len) {
     if (len === undefined) len = 5;
     let input = form.querySelector(`[name=${name}]`);
     let txt = input.value;
 
+    const errMsgs = input.closest("td").querySelectorAll("p");
+    if (errMsgs.length > 0) input.closest("td").querySelector("p").remove();
     if (txt.length >= len && txt != "") {
-        const errMsgs = input.closest("td").querySelectorAll("p");
-        if (errMsgs.length > 0) input.closest("td").querySelector("p").remove();
         input.classList.remove("text-error");
         return true;
     } else {
-        const errMsgs = input.closest("td").querySelectorAll("p");
-        if (errMsgs.length > 0) input.closest("td").querySelector("p").remove();
-
         const errMsg = document.createElement("p");
         errMsg.append(`입력항목을 ${len}글자 이상 입력하세요`);
         input.closest("td").append(errMsg);
@@ -38,19 +37,19 @@ function isTxt(name, len) {
     }
 }
 
+// email 인증 함수 정의
 function isEmail(name) {
     let input = form.querySelector(`[name=${name}]`);
     let txt = input.value;
 
+    const errMsgs = input.closest("td").querySelectorAll("p");
+    if (errMsgs.length > 0) input.closest("td").querySelector("p").remove();
+
+    // @ 문자가 포함되어있는지
     if (/@/.test(txt)) {
-        const errMsgs = input.closest("td").querySelectorAll("p");
-        if (errMsgs.length > 0) input.closest("td").querySelector("p").remove();
         input.classList.remove("text-error");
         return true;
     } else {
-        const errMsgs = input.closest("td").querySelectorAll("p");
-        if (errMsgs.length > 0) input.closest("td").querySelector("p").remove();
-
         const errMsg = document.createElement("p");
         errMsg.append("@를 포함한 전체 이메일 주소를 입력해주세요");
         input.closest("td").append(errMsg);
@@ -59,30 +58,30 @@ function isEmail(name) {
     }
 }
 
+// 체크박스,라디오박스 인증 함수 정의
 function isCheck(name) {
     let inputs = form.querySelectorAll(`[name=${name}]`);
     let isChecked = false;
 
+    // 체크되어있는 요소 확인
     for (let el of inputs) {
         if (el.checked) isChecked = true;
     }
 
+    const errMsgs = inputs[0].closest("td").querySelectorAll("p");
+    if (errMsgs.length > 0) inputs[0].closest("td").querySelector("p").remove();
+
     if (isChecked) {
-        const errMsgs = inputs[0].closest("td").querySelectorAll("p");
-        if (errMsgs.length > 0) inputs[0].closest("td").querySelector("p").remove();
         return true;
     } else {
-        const errMsgs = inputs[0].closest("td").querySelectorAll("p");
-        if (errMsgs.length > 0) inputs[0].closest("td").querySelector("p").remove();
-
         const errMsg = document.createElement("p");
         errMsg.append("필수입력항목을 체크해주세요");
         inputs[0].closest("td").append(errMsg);
-
         return false;
     }
 }
 
+// 비밀번호 인증 함수 정의
 function isPwd(name1, name2, len) {
     let pwd1 = form.querySelector(`[name=${name1}]`);
     let pwd2 = form.querySelector(`[name=${name2}]`);
@@ -93,24 +92,17 @@ function isPwd(name1, name2, len) {
     const eng = /[a-zA-Z]/;
     const spc = /[~!@#$%^&*()_+]/;
 
+    const errMsgs = pwd1.closest("td").querySelectorAll("p");
+    const errMsgs2 = pwd2.closest("td").querySelectorAll("p");
+    if (errMsgs.length > 0) pwd1.closest("td").querySelector("p").remove();
+    if (errMsgs2.length > 0) pwd2.closest("td").querySelector("p").remove();
+
+    // 비밀번호와 재입력 값이 값은지, 문자의 수가 5개이상, 공란이 아니고, 숫자포함, 영문자포함, 특수문자포함
     if (pwd1_val === pwd2_val && pwd1_val.length >= len && num.test(pwd1_val) && eng.test(pwd1_val) && spc.test(pwd1_val)) {
-        const errMsgs = pwd1.closest("td").querySelectorAll("p");
-        const errMsgs2 = pwd2.closest("td").querySelectorAll("p");
-        if (errMsgs.length > 0) {
-            pwd1.closest("td").querySelector("p").remove();
-        }
         pwd1.classList.remove("text-error");
-        if (errMsgs2.length > 0) {
-            pwd2.closest("td").querySelector("p").remove();
-        }
         pwd2.classList.remove("text-error");
         return true;
     } else {
-        const errMsgs = pwd1.closest("td").querySelectorAll("p");
-        const errMsgs2 = pwd2.closest("td").querySelectorAll("p");
-        if (errMsgs.length > 0) pwd1.closest("td").querySelector("p").remove();
-        if (errMsgs2.length > 0) pwd2.closest("td").querySelector("p").remove();
-
         const errMsg = document.createElement("p");
         const errMsg2 = document.createElement("p");
         errMsg.append(`비밀번호는 ${len}글자 이상 영문, 숫자, 특수문자를 포함하여 입력하세요`);
