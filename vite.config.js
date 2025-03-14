dbc9135bdbece8ce8c0bec6910657d245805a1b9
minhyepark <minhyepark.dev@gmail.com>
2025-03-14 23:28:12 +0900

    edit vite config

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,18 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/portfolio_interior/', // GitHub Pages 서브 디렉토리 설정
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        login: 'login.html',
+        join: 'join.html',
+        community: 'community.html',
+        department: 'department.html',
+        gallery: 'gallery.html',
+        join_result: 'join_result.html',
+        location: 'location.html',
+      },
+    },
+  },
 });
